fd2b47025f56c78a9096a31296d50cbb9209bc51
riduan91 <riduan91@gmail.com>
2017-09-04 18:53:22 +0200

    C

--- source/WebServer/RunServer.py
+++ source/WebServer/RunServer.py
@@ -7,12 +7,17 @@ app = Flask(__name__)
 SOURCE_DIR = "../"
 import sys
 sys.path.append(SOURCE_DIR)
-import Group
+import Group, Current
+
+#############INDEX######################
 
 @app.route("/")
 def index():
     return render_template("index.html")
     
+
+#---------------------PREPARATION-META---------------------
+
 @app.route("/new_meta", methods = ['POST', 'GET'])
 def new_meta():
     return render_template("admin_preparation.html")
@@ -22,37 +27,78 @@ def send_meta():
     if request.method == 'POST':
         parameters = request.form
         Group.updateGroups(parameters)
-        res = Group.fetchGroups()
+        res = Group.fetchGroups(parameters["series"])
         
-        return render_template("admin_preparation_question.html", result = (len(res), res))
+        return render_template("admin_preparation_question.html", result = (len(res), res, parameters["series"]))
     else:
         return render_template("admin_preparation.html")
-
-@app.route("/send_questions", methods = ['POST', 'GET'])
-def send_questions():
-    if request.method == 'POST':
-        parameters = request.form
-        res1 = Group.fetchGroups()
-        Group.updateQuestions(parameters)
-        res2 = Group.fetchQuestions()
-        #res = parameters
-        return render_template("admin_question_information.html", result = (len(res1), res1, res2))
-    else:
-        res1 = Group.fetchGroups()
-        res2 = Group.fetchQuestions()
-        return render_template("admin_question_information.html")
-
-@app.route("/view_meta", methods = ['POST', 'GET'])
-def view_meta():
+        
+@app.route("/update_meta", methods = ['POST'])
+def update_meta():
     if request.method == 'POST':
         parameters = request.form
         Group.updateGroups(parameters)
-        res = Group.fetchGroups()
+        res1 = Group.fetchGroups(parameters["series"])        
+        return render_template("admin_group_information.html", result = (len(res1), res1, parameters["series"]))
         
-        return render_template("admin_get_info_group.html", result = (len(res), res))
+
+@app.route("/view_meta", methods = ['POST'])
+def view_meta():
+    if request.method == 'POST':
+        parameters = request.form
+        res1 = Group.fetchGroups(parameters["series"])    
+        return render_template("admin_group_information.html", result = (len(res1), res1, parameters["series"]))
+
+
+#---------------------PREPARATION-QUESTION---------------------
+
+@app.route("/send_questions", methods = ['POST'])
+def send_questions():
+    if request.method == 'POST':
+        parameters = request.form
+        res1 = Group.fetchGroups(parameters["series"])
+        Group.updateQuestions(parameters["series"], parameters)
+        res2 = Group.fetchQuestions(parameters["series"])
+        #res = parameters
+        return render_template("admin_question_information.html", result = (len(res1), res1, res2, parameters["series"]))
+
+@app.route("/view_general", methods = ['GET'])
+def view_general():
+    res = Group.getSeries();
+    return render_template("admin_all_series.html", result = res)
+
+
+@app.route("/view_questions", methods = ['POST'])
+def view_questions():
+    if request.method == 'POST':
+        parameters = request.form
+        res1 = Group.fetchGroups(parameters["series"])   
+        res2 = Group.fetchQuestions(parameters["series"])    
+        return render_template("admin_question_information.html", result = (len(res1), res1, res2, parameters["series"]))
+
+
+#---------------------ADMIN-START---------------------
+
+@app.route("/admin_start", methods = ['GET', 'POST'])
+def admin_start():
+    if request.method == 'POST':
+        parameters = request.form
+        res1 = Group.getSeries();
+        
+        Current.updateCurrentSeriesName(parameters["series"])
+        Current.updateCurrentGroup(0)
+        Current.updateCurrentPlayers(parameters["players"])        
+        
+        res2 = Group.fetchCurrent()
+        return render_template("admin_start_1.html", result = (res1, res2))
     else:
-        res = Group.fetchGroups()
-        return render_template("admin_get_info_group.html", result = (len(res), res))
+        res = Group.getSeries();
+        return render_template("admin_start_0.html", result = (res))
+
+
+
+
+
 
 HOST = '0.0.0.0'
 PORT = 8803
